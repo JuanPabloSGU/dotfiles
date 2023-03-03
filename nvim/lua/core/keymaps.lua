7e021559b60b237f930e5eb6f1f1ebfa0c186819
JuanPabloSGU <jsanc016@uottawa.ca>
2023-03-03 12:16:45 -0500

    feat: add lsp keymaps for whick-key to find
    Signed-off-by: JuanPabloSGU <jsanc016@uottawa.ca>

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -52,3 +52,17 @@ keymap.set("v", "<leader>/", "<ESC><cmd>lua require('Comment.api').toggle.linewi
 
 -- LazyGit
 keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
+
+-- LSP
+keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>") -- show definition, references
+keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>") -- got to declaration
+keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>") -- see definition and make edits in window
+keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>") -- go to implementation
+keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>") -- see available code actions
+keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>") -- smart rename
+keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>") -- show  diagnostics for line
+keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>") -- show diagnostics for cursor
+keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>") -- jump to previous diagnostic in buffer
+keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>") -- jump to next diagnostic in buffer
+keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>") -- show documentation for what is under cursor
+keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>") -- see outline on right hand side
